0a6dccedb5ec34005d7478039b7774f7d1915a03
wintor12 <wintor12@gmail.com>
2017-07-28 01:48:20 -0400

    .

--- util.py
+++
@@ -1,6 +0,0 @@
-
-def loadFeatures():
-    return np.loadtxt('data/feature_train'),np.loadtxt('data/feature_val')
-
-def loadUpvote():
-    return np.loadtxt('data/y_train'),np.loadtxt('data/y_val')
