d18bbdd234153d5ee5bf458fe6363b897f26475e
wintor12 <wintor12@gmail.com>
2017-08-01 23:32:29 -0400

    create dataset.py

--- dataset.py
+++ dataset.py
@@ -8,22 +8,27 @@ PAD_WORD = "<PAD>"
 
 
 class StoryDataset(torchtext.data.Dataset):
+
     def __init__(self, fields, src_path, question_path,
-                 feature_path=None, tgt_path=None, opt=None, **kwargs):
+                 feature_path, tgt_path, opt=None, **kwargs):
         examples = []
-        src_words = []
-        q_words = []
         self.src_vocabs = []
-        with codecs.open(src_path, 'r', 'utf-8') as src_file, codecs.open(question_path, 'r', 'utf-8') as q_file:
-            for i, (src_line, q_line) in enumerate(zip(src_file, q_file)):
+        with codecs.open(src_path, 'r', 'utf-8') as src_file, \
+             codecs.open(question_path, 'r', 'utf-8') as q_file, \
+             codecs.open(feature_path, 'r', 'utf-8') as f_file, \
+             codecs.open(tgt_path, 'r', 'utf-8') as t_file:
+            for i, (src_line, q_line, f_line, t_line) in enumerate(
+                    zip(src_file, q_file, f_file, t_file)):
+
                 src = src_line.strip().split()
                 question = q_line.strip().split()
-                d = {'src': src, 'question': question, 'indices': i}
+                feature = f_line.strip().split()
+                feature = [float(x) for x in feature]
+                tgt = t_line.strip()
+                d = {'src': src, 'question': question, 'indices': i,
+                     'feature': feature, 'tgt': tgt}
                 examples.append(d)
-                src_words.append(src)
-                q_words.append(question)
-        print(len(src_words))
-        print(len(q_words))
+
         keys = examples[0].keys()
         fields = [(k, fields[k]) for k in keys]
         examples = list([torchtext.data.Example.fromlist([ex[k] for k in keys], fields)
@@ -32,30 +37,46 @@ class StoryDataset(torchtext.data.Dataset):
         super(StoryDataset, self).__init__(examples, fields)
 
     @staticmethod
+    def sort_key(ex):
+        "Sort in reverse size order"
+        return -len(ex.src)
+
+    @staticmethod
     def get_fields():
         fields = {}
         fields['src'] = torchtext.data.Field(
             pad_token=PAD_WORD,
             include_lengths=True)
+
         # question and src text share the same field
         fields['question'] = fields['src']
+
         fields['indices'] = torchtext.data.Field(
             use_vocab=False,
             tensor_type=torch.LongTensor,
             sequential=False)
+
+        fields['feature'] = torchtext.data.Field(
+            use_vocab=False,
+            tensor_type=torch.FloatTensor,
+            sequential=False)
+
+        fields['tgt'] = torchtext.data.Field(
+            use_vocab=False,
+            tensor_type=torch.FloatTensor,
+            sequential=False)
+
         return fields
 
     @staticmethod
     def build_vocab(train, opt):
         fields = train.fields
-        fields['src'].build_vocab(train, max_size=20000)
+        fields['src'].build_vocab(train, max_size=opt.src_vocab_size)
 
+    def __getstate__(self):
+        "Need this for pickle save"
+        return self.__dict__
 
-def main():
-    fields = StoryDataset.get_fields()
-    train = StoryDataset(fields, 'data/s_train', 'data/q_train')
-    print(train[0].src, train[0].question, train[0].indices)
-
-
-if __name__ == "__main__":
-    main()
+    def __setstate__(self, d):
+        "Need this for pickle load"
+        self.__dict__.update(d)
--- train.py
+++ train.py
@@ -5,24 +5,30 @@ from torch.utils.data import DataLoader, TensorDataset
 import torch.optim as optim
 import torch.nn as nn
 from torch.autograd import Variable
+from torchtext.data import BucketIterator
 import utils
 import models
 import sys
+import dill
 from datetime import datetime
 from pycrayon import CrayonClient
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--batchSize', type=int, default=32, help='input batch size')
-parser.add_argument('--epoch', type=int, default=25, help='number of epochs to train for')
-parser.add_argument('--cuda', action='store_true', help='enables cuda')
+parser.add_argument('--gpus', default=[], nargs='+', type=int,
+                    help='Use CUDA on the listed devices')
 parser.add_argument('--log-interval', type=int, default=100, metavar='N',
                     help='how many batches to wait before logging training status')
+
+parser.add_argument('--batch_size', type=int, default=32, help='input batch size')
+parser.add_argument('--epoch', type=int, default=25, help='number of epochs to train for')
 parser.add_argument('--hidden1', type=int, default=128)
 parser.add_argument('--hidden2', type=int, default=128)
 parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
                     help='learning rate (default: 0.1)')
 parser.add_argument('--dropout', type=float, default=0.3, help='dropout between layers')
+
+parser.add_argument('--data', default='./data/', help='the path to load data')
 parser.add_argument('--save', default='./data/model', help='the path to save model files')
 parser.add_argument('--crayon', action='store_true', help='visualization')
 
@@ -94,41 +100,55 @@ def dataLoad():
 
 
 def main():
-    train_loader, val_loader, num_features = dataLoad()
-    tb_train, tb_valid = None, None
-    if opt.crayon:
-        tb_client = CrayonClient()
-        tb_name = '{}-{}'.format(datetime.now().strftime("%y%m%d-%H%M%S"),
-                                 opt.save)
-        tb_train = tb_client.create_experiment('{}/train'.format(tb_name))
-        tb_valid = tb_client.create_experiment('{}/valid'.format(tb_name))
-    model = models.Fc(num_features, opt.hidden1, opt.hidden2, opt.dropout)
-    criterion = nn.MSELoss()
-    if opt.cuda:
-        model.cuda()
-        criterion.cuda()
-    lr = opt.lr
-    loss_old, loss, loss_best = sys.maxint, 0, sys.maxint
-    for e in range(1, opt.epoch + 1):
-        optimizer = optim.SGD(model.parameters(), lr=lr)
-        train(model, train_loader, e, optimizer, criterion, tb_train)
-        loss = val(model, val_loader, e, criterion, tb_valid)
-        print('LR: \t: {:.6f}'.format(lr))
-        if loss.data[0] < loss_old:
-            if loss.data[0] < loss_best:
-                loss_best = loss.data[0]
-                checkpoint = {
-                    'model': model.state_dict(),
-                    'opt': opt,
-                    'epoch': e,
-                    'optim': optimizer
-                }
-                torch.save(checkpoint,
-                           '%s_loss_%.5f_e%d.pt' % (opt.save, loss_best, e))
-            loss_old = loss.data[0]
-        else:
-            lr = lr * 0.5
-        print('')
+    train = torch.load(opt.data + 'train.pt', pickle_module=dill)
+    fields = torch.load(opt.data + 'fields.pt', pickle_module=dill)
+    valid = torch.load(opt.data + 'valid.pt', pickle_module=dill)
+    fields = dict(fields)
+    print(' * vocabulary size. source = %d *'%len(fields['src'].vocab))
+    train = BucketIterator(
+            dataset=train, batch_size=opt.batch_size,
+            device=opt.gpus[0] if opt.gpus else -1,
+            repeat=False)
+    print(len(train))
+    
+    
+
+# def main():
+#    train_loader, val_loader, num_features = dataLoad()
+#    tb_train, tb_valid = None, None
+#    if opt.crayon:
+#        tb_client = CrayonClient()
+#        tb_name = '{}-{}'.format(datetime.now().strftime("%y%m%d-%H%M%S"),
+#                                 opt.save)
+#        tb_train = tb_client.create_experiment('{}/train'.format(tb_name))
+#        tb_valid = tb_client.create_experiment('{}/valid'.format(tb_name))
+#    model = models.Fc(num_features, opt.hidden1, opt.hidden2, opt.dropout)
+#    criterion = nn.MSELoss()
+#    if opt.cuda:
+#        model.cuda()
+#        criterion.cuda()
+#    lr = opt.lr
+#    loss_old, loss, loss_best = sys.maxint, 0, sys.maxint
+#    for e in range(1, opt.epoch + 1):
+#        optimizer = optim.SGD(model.parameters(), lr=lr)
+#        train(model, train_loader, e, optimizer, criterion, tb_train)
+#        loss = val(model, val_loader, e, criterion, tb_valid)
+#        print('LR: \t: {:.6f}'.format(lr))
+#        if loss.data[0] < loss_old:
+#            if loss.data[0] < loss_best:
+#                loss_best = loss.data[0]
+#                checkpoint = {
+#                    'model': model.state_dict(),
+#                    'opt': opt,
+#                    'epoch': e,
+#                    'optim': optimizer
+#                }
+#                torch.save(checkpoint,
+#                           '%s_loss_%.5f_e%d.pt' % (opt.save, loss_best, e))
+#            loss_old = loss.data[0]
+#        else:
+#            lr = lr * 0.5
+#        print('')
 
 
 if __name__ == "__main__":
