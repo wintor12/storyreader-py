dcad50b679a390676d9550bb9a61af42d3d88f65
tikael1011 <tikael1011@users.noreply.github.com>
2017-08-03 07:43:49 -0400

    Update tenreader.py
    add evenly split, add tail blank

--- tenreader.py
+++ tenreader.py
@@ -8,47 +8,69 @@ class Tenfoldreader:
     Different methods give different output format, either list of list or list.
     '''
     
-    def returnlist(self,strarr):
+    def returnlist(self,strlist):
         # @param string list
         # @return string list
         # the length of return will be either 360 or the length of original list.
-        hf = len(strarr) // 2 # Python 2 user no need to change this part, this is for Python 3
+        hf = len(strlist) // 2 # Python 2 user no need to change this part, this is for Python 3
         if (hf >= 180):
             res = [None] * 360
-            for i,string_ in enumerate (strarr[hf-180:hf+180]):
+            for i,string_ in enumerate (strlist[hf-180:hf+180]):
                 res[i] = string_
             return res
         else:
             return strarr
     
-    def returntupleoflist(self,strarr):
+    def returnlistoflist(self,strlist):
         # @param string list
         # @return tuple of string list
         # the length of return of always be 10.
         # but for the item length, it maybe less than 36.
-        hf = len(strarr) // 2
+        hf = len(strlist) // 2
         res = []
         if (hf >= 180):
             tmp = []
-            for i, string_ in enumerate (strarr[hf-180:hf+180]):
+            for i, string_ in enumerate (strlist[hf-180:hf+180]):
                 tmp.append(string_)
                 if ((i +1)% 36 == 0):
                     res.append(tmp)
                     tmp = []
             return res
         else:
-            args = [iter(strarr)] * 10
-            return list(zip_longest(*args))
+            res = list(self.spliteven(strlist,10))
+            return self.resizeTo36each(res)
         
-    def chunks(self,iterableitem, n):
-        '''Yield successive n-sized chunks from iterableitem.'''
-        for i in range(0, len(iterableitem), n):
-            yield iterableitem[i:i + n]
-            
-
+#     def chunks(self,iterableitem, n):
+#         '''Yield successive n-sized chunks from iterableitem.'''
+#         for i in range(0, len(iterableitem), n):
+#             yield iterableitem[i:i + n]
+    
+#     def grouper(n, iterable, fillvalue=None):
+#     "grouper(3, 'ABCDEFG', 'x') --> ABC DEF Gxx"
+#         args = [iter(iterable)] * n
+#         return izip_longest(fillvalue=fillvalue, *args)
+    
+    def spliteven(self,inputlist, n):
+        k, m = divmod(len(inputlist), n)
+        return (inputlist[i * k + min(i, m):(i + 1) * k + min(i + 1, m)] for i in range(n))
+    
+    def resizeTo36each(self,inputlist):
+        '''resize every element of the list to 36'''
+        # @param the length of inputlist is 10
+        # @return each element will have size 36, thus the 'total' will be 360
+        # add tail'blank' to element of which size is less than 36
+        # add both head && tail 'blank' is silly, not good
+        for element in inputlist:
+            if (len(element)<36):
+                while (len(element)<36):
+                    element.append(' ')
+        return inputlist
 
     
 a = ['1','2','3','4','5','6','7','8','9','10','11','12']
+c = list(range(36))
 print(type(a))
 tf = Tenfoldreader()
-b = tf.returntupleoflist(a)
+# b = tf.returntupleoflist(c)
+# print(len(b))
+d = tf.returnlistoflist(c)
